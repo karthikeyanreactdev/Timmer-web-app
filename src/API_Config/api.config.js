c646cf2be6644c03931e18a21c2db007b165bebd
Karthikeyan D <81141000+karthikeyanreactdev@users.noreply.github.com>
2021-05-16 14:20:13 +0530

    Update api.config.js

--- src/API_Config/api.config.js
+++ src/API_Config/api.config.js
@@ -1,6 +1,6 @@
 const API_PATH = {		
-		 API_ROOT: 'http://localhost:5000' 
-		//API_ROOT: 'https://nativenodejs.herokuapp.com' 				
+// API_ROOT: 'http://localhost:5000' 
+		API_ROOT: 'https://nativeapi.herokuapp.com' 				
 };
 
 export default API_PATH;
